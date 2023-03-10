c210ad7f00845a4a931d1ea7d8e945f82f420f4f
Emmanuel Owolabi <owoemma59@gmail.com>
2023-03-10 09:43:13 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import store from "./store/index";
 import "./index.css";
 import App from "./App";
-import { PDFViewer } from "@react-pdf/renderer";
 
 ReactDOM.render(
   <Provider store={store}>
